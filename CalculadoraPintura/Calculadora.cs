2184a677be0ff53093a7936985c21665dc788804
Gislaine Mendes da Rocha <gislainemrocha@gmail.com>
2020-04-26 18:55:25 -0300

    first commit

---
+++ CalculadoraPintura/Calculadora.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPintura
+{
+    class Calculadora
+    {
+        private double areaParedes; // utilizar modificador de acesso para determinar a visibilidade do atributo
+        private double areaTeto;
+
+        public double CalcularAreaParedes(double largura, double profundidade, double altura)
+        {
+            areaParedes = 2 * (largura + profundidade) * altura;  // case sensitive, informar os parametros iguais dentro do método
+            return areaParedes;
+        }
+
+        public double CalcularAreaTeto(double largura, double profundidade)
+        {
+            areaTeto = largura * profundidade; // case sensitive, informar os parametros iguais dentro do método
+            return areaTeto;
+        }
+
+        public double CalcularLitragemNecessaria()
+        {
+            return (areaParedes + areaTeto) / 10;//10 é o valor estimado de rendimento da tinta
+        }
+    }
+}
---
+++ CalculadoraPintura/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPintura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a largura do cômodo?");
+            double largura = (double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Qual a profundidade do cômodo?");
+            double profundidade = (double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("A área das paredes é: ");
+            const double Altura = 2.9; //o prof estipulou 2.9 de altura;
+
+            Calculadora calculadora = new Calculadora(); // instancia da classe calculadora
+            Console.WriteLine(calculadora.CalcularAreaParedes(largura, profundidade, Altura)); //atributo areaParede é da classe calculadora
+
+            Console.WriteLine("A área do teto é: ");
+            Console.WriteLine(calculadora.CalcularAreaTeto(largura,profundidade));
+
+            Console.WriteLine("A litragem de tinta necessária é: ");
+            Console.WriteLine(calculadora.CalcularLitragemNecessaria()); 
+            Console.ReadLine();
+        }
+    }
+}
